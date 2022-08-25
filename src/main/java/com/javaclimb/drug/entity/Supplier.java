140ae932fe2bdcc77715de5f98e1294c3496874f
fugther <G1570760724@163.com>
2022-08-25 23:52:41 +0800

    初始化

---
+++ src/main/java/com/javaclimb/drug/entity/Supplier.java
@@ -0,0 +1,63 @@
+package com.javaclimb.drug.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 供应商
+ */
+@Data
+@TableName(value = "supplier")
+public class Supplier implements Serializable {
+    /*主键*/
+    @TableField(value = "id")
+    @TableId(value = "id",type = IdType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    /*供应商名称*/
+    private String name;
+
+    /*供应商描述*/
+    private String content;
+
+    /*创建时间*/
+    private Date createtime;
+}
